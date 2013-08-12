5fe4eaf930771e5bad43c7d4fd93258593738511
Matthias Bolte <matthias@tinkerforge.com>
2013-08-12 16:51:33 +0200

    shell: Explain magic value

--- software/examples/shell/example-simple.sh
+++ software/examples/shell/example-simple.sh
@@ -4,5 +4,5 @@
 # change to your UID
 uid=XYZ
 
-# turn pins 0, 3 high and pins 1, 2 low
+# turn pins 0, 3 high and pins 1, 2 low: (1 << 0) | (1 << 3) = 9
 tinkerforge call industrial-digital-out-4-bricklet $uid set-value 9

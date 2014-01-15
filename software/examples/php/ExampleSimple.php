339f97f91fc65b8fb4baa0fb5b8b8a2dd8f31e68
Matthias Bolte <matthias@tinkerforge.com>
2014-01-15 11:19:04 +0100

    php: Use const for constant values

--- software/examples/php/ExampleSimple.php
+++ software/examples/php/ExampleSimple.php
@@ -6,14 +6,14 @@ require_once('Tinkerforge/BrickletIndustrialDigitalOut4.php');
 use Tinkerforge\IPConnection;
 use Tinkerforge\BrickletIndustrialDigitalOut4;
 
-$host = 'localhost';
-$port = 4223;
-$uid = 'xyz'; // Change to your UID
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = 'xyz'; // Change to your UID
 
 $ipcon = new IPConnection(); // Create IP connection
-$ido4 = new BrickletIndustrialDigitalOut4($uid, $ipcon); // Create device object
+$ido4 = new BrickletIndustrialDigitalOut4(UID, $ipcon); // Create device object
 
-$ipcon->connect($host, $port); // Connect to brickd
+$ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
 // Turn pins alternating high/low for 10 times with 100 ms delay

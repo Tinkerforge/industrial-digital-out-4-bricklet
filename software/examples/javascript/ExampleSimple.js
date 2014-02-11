fc41f4d2cd5c531cff16aebe5c97fce1f0356582
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 16:46:07 +0100

    javascript: short examples

--- software/examples/javascript/ExampleSimple.js
+++ software/examples/javascript/ExampleSimple.js
@@ -10,9 +10,7 @@ var ido4 = new BrickletIndustrialDigitalOut4(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
-            console.log('Error: Already connected');        
-        }
+        console.log('Error: '+error);        
     }
 );// Connect to brickd
 
@@ -25,14 +23,10 @@ ipcon.on(IPConnection.CALLBACK_CONNECTED,
 );
 
 console.log("Press any key to exit ...");
-process.stdin.on('data', function(data) {
-	    ipcon.disconnect(
-            function(error) {
-                if(error === IPConnection.ERROR_NOT_CONNECTED) {
-                    console.log('Error: Not connected');        
-                }
-            }
-        );
-process.exit(0);
-});
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
 

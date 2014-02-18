a94194fccda05a1d8d0def53fdbaaac08792a561
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-18 16:02:18 +0100

    javascript: examples fixed

--- software/examples/javascript/ExampleSimple.js
+++ software/examples/javascript/ExampleSimple.js
@@ -1,12 +1,11 @@
-var IPConnection = require('Tinkerforge/IPConnection');
-var BrickletIndustrialDigitalOut4 = require('Tinkerforge/BrickletIndustrialDigitalOut4');
+var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
 var UID = 'cmN';// Change to your UID
 
-var ipcon = new IPConnection();// Create IP connection
-var ido4 = new BrickletIndustrialDigitalOut4(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection();// Create IP connection
+var ido4 = new Tinkerforge.BrickletIndustrialDigitalOut4(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
@@ -15,7 +14,7 @@ ipcon.connect(HOST, PORT,
 );// Connect to brickd
 
 // Don't use device before ipcon is connected
-ipcon.on(IPConnection.CALLBACK_CONNECTED,
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         //Turn on first two outputs and turn off last two outputs
         ido4.setValue(3);     

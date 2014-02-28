33949defc2d6d4bbfc409570288226cd19f4cc16
Matthias Bolte <matthias@tinkerforge.com>
2014-02-28 19:17:10 +0100

    javascript: Break long lines and cleanup whitespace

--- software/examples/javascript/ExampleSimple.js
+++ software/examples/javascript/ExampleSimple.js
@@ -2,22 +2,22 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = 'cmN';// Change to your UID
+var UID = 'cmN'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var ido4 = new Tinkerforge.BrickletIndustrialDigitalOut4(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var ido4 = new Tinkerforge.BrickletIndustrialDigitalOut4(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
-        //Turn on first two outputs and turn off last two outputs
-        ido4.setValue(3);     
+        // Turn on first two outputs and turn off last two outputs
+        ido4.setValue(3);
     }
 );
 
@@ -28,4 +28,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-

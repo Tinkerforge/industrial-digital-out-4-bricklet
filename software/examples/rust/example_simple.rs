22c1504870b166e8c6f67542294007b025950aed
Erik Fleckstein <erik@tinkerforge.com>
2018-11-06 15:53:13 +0100

    Add rust examples

---
+++ software/examples/rust/example_simple.rs
@@ -0,0 +1,32 @@
+use std::{error::Error, io, thread, time::Duration};
+use tinkerforge::{industrial_digital_out_4_bricklet::*, ipconnection::IpConnection};
+
+const HOST: &str = "127.0.0.1";
+const PORT: u16 = 4223;
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Digital Out 4 Bricklet
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ipcon = IpConnection::new(); // Create IP connection
+    let industrial_digital_out_4_bricklet = IndustrialDigitalOut4Bricklet::new(UID, &ipcon); // Create device object
+
+    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
+                                        // Don't use device before ipcon is connected
+
+    // Set pins alternating high/low 10 times with 100ms delay
+    for i in 0..10 {
+        thread::sleep(Duration::from_millis(100));
+        industrial_digital_out_4_bricklet.set_value(1 << 0);
+        thread::sleep(Duration::from_millis(100));
+        industrial_digital_out_4_bricklet.set_value(1 << 1);
+        thread::sleep(Duration::from_millis(100));
+        industrial_digital_out_4_bricklet.set_value(1 << 2);
+        thread::sleep(Duration::from_millis(100));
+        industrial_digital_out_4_bricklet.set_value(1 << 3);
+    }
+
+    println!("Press enter to exit.");
+    let mut _input = String::new();
+    io::stdin().read_line(&mut _input)?;
+    ipcon.disconnect();
+    Ok(())
+}

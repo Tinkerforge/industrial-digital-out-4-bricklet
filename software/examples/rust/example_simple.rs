707fc285e41fb7836f933267d843654ae18c0bb3
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:22:06 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,27 +1,27 @@
 use std::{error::Error, io, thread, time::Duration};
-use tinkerforge::{industrial_digital_out_4_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_digital_out_4_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Digital Out 4 Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Digital Out 4 Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_digital_out_4_bricklet = IndustrialDigitalOut4Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ido4 = IndustrialDigitalOut4Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Set pins alternating high/low 10 times with 100ms delay
     for i in 0..10 {
         thread::sleep(Duration::from_millis(100));
-        industrial_digital_out_4_bricklet.set_value(1 << 0);
+        ido4.set_value(1 << 0);
         thread::sleep(Duration::from_millis(100));
-        industrial_digital_out_4_bricklet.set_value(1 << 1);
+        ido4.set_value(1 << 1);
         thread::sleep(Duration::from_millis(100));
-        industrial_digital_out_4_bricklet.set_value(1 << 2);
+        ido4.set_value(1 << 2);
         thread::sleep(Duration::from_millis(100));
-        industrial_digital_out_4_bricklet.set_value(1 << 3);
+        ido4.set_value(1 << 3);
     }
 
     println!("Press enter to exit.");

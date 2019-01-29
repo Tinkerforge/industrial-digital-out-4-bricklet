06582bfc17fdc30b8d046029ea15c38f95ca62b1
Erik Fleckstein <erik@tinkerforge.com>
2019-01-29 16:57:15 +0100

    rust: Fix unused loop index.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -13,7 +13,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                                           // Don't use device before ipcon is connected.
 
     // Set pins alternating high/low 10 times with 100ms delay
-    for i in 0..10 {
+    for _i in 0..10 {
         thread::sleep(Duration::from_millis(100));
         ido4.set_value(1 << 0);
         thread::sleep(Duration::from_millis(100));
